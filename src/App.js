3798b8febee28aed60b2dc6a356423856a932a58
mrinal <mrinalspec@gmail.com>
2022-09-11 20:04:33 +0530

    contact Page added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { lazy, Suspense } from 'react';
-import './App.css';
+import React, { lazy, Suspense } from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './Main/Navbar';
-import Loader from './Shared/Loader';
-import Spinner from 'react-bootstrap/Spinner';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Header from "./Main/Navbar";
+import Loader from "./Shared/Loader";
+import Spinner from "react-bootstrap/Spinner";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Intro = lazy(() => import("./Components/Intro"));
-const Timeline = lazy(() => import("./Components/Timeline"))
-const Contact = lazy(() => import("./Components/Contact"))
-const Skills = lazy(() => import("./Components/Skill"))
-const About = lazy(() => import("./Components/About"))
-
-
-
+const Timeline = lazy(() => import("./Components/Timeline"));
+const Contact = lazy(() => import("./Components/Contact"));
+const Skills = lazy(() => import("./Components/Skill"));
+const About = lazy(() => import("./Components/About"));
 
 function App() {
-
   return (
     <div className="App">
-     <BrowserRouter>
-          <Header/>
-          <Suspense  fallback={<Loader/>}>
-        <Routes>
-          <Route path="/" element={<Intro />} />
-          <Route path="/career" element={<Timeline />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skill" element={<Skills  height={745} width={1440}/>} />
-        </Routes>
-        </Suspense> 
+      <BrowserRouter>
+        <Header />
+        <Suspense fallback={<Loader />}>
+          <Routes className="headerPlaced">
+            <Route path="/" element={<Intro />} />
+            <Route path="/career" element={<Timeline />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/about"
+              element={<About style={{ margin: "200px" }} />}
+            />
+            <Route
+              path="/skill"
+              element={<Skills height={745} width={1440} />}
+            />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
