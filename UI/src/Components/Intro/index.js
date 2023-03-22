aa54f0c995f1ffc4edfaa0cd7b1a35b2a9718dfd
c2-mrinal <mrinalspec@gmail.com>
2023-03-22 22:40:38 +0530

    into section change

--- UI/src/Components/Intro/index.js
+++ UI/src/Components/Intro/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../../Shared/Button";
 import "./intro.css";
+import Nav from "react-bootstrap/Nav";
 
 function Intro() {
   const downloadResume = () => {
@@ -15,13 +16,43 @@ function Intro() {
     // });
   };
   return (
-    <div className="backgroundImage">
-      <div className="downloadResumeButton">
-        <Button
-          className="btn btn-secondary"
-          name="Download Resume"
-          onClick={downloadResume}
-        />
+    <div>
+      <div className="backgroundImage">
+        <div className="downloadResumeButton">
+          <Button
+            className="btn btn-secondary"
+            name="Download Resume"
+            onClick={downloadResume}
+          />
+        </div>
+      </div>
+      <div className="intoSection">
+        <div className="careerbg">
+          <h3>
+            <Nav.Link href="/career">Career</Nav.Link>
+          </h3>
+        </div>
+      </div>
+      <div className="intoSection">
+        <div className="skillbg">
+          <h3>
+            <Nav.Link href="/skill">Skills</Nav.Link>
+          </h3>
+        </div>
+      </div>
+      <div className="intoSection">
+        <div className="aboutbg">
+          <h3>
+            <Nav.Link href="/about">About</Nav.Link>
+          </h3>
+        </div>
+      </div>
+      <div className="intoSection">
+        <div className="contactbg">
+          <h3>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </h3>
+        </div>
       </div>
     </div>
   );
