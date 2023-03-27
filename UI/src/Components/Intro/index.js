0489db34cd66de80e449225720abef644dda1e58
c2-mrinal <mrinalspec@gmail.com>
2023-03-27 08:48:30 +0530

    into page

--- UI/src/Components/Intro/index.js
+++ UI/src/Components/Intro/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Button from "../../Shared/Button";
 import "./intro.css";
+import invertedArrowPointing from "../../image/invertedArrowPointing.gif";
+import backgroundMain from "../../image/backgroundMain.mp4";
 import Nav from "react-bootstrap/Nav";
 
 function Intro() {
@@ -13,15 +15,35 @@ function Intro() {
   };
   return (
     <div>
-      <div className="backgroundImage">
-        <div className="downloadResumeButton">
+      <div className="myVideo">
+        <video autoPlay muted loop width="100%" height="100%">
+          <source src={backgroundMain} type="video/mp4" />
+        </video>
+      </div>
+      <section className=" intoSection">
+        <div className="resumeDownloadContainer">
           <Button
-            className="btn btn-secondary"
-            name="Download Resume"
+            className="btn btn-secondary downloadResumeButton"
+            name="Resume"
             onClick={downloadResume}
+            icon={"fa-1x fa fa-download"}
+          />
+          <img
+            src={invertedArrowPointing}
+            className="pointingArrow"
+            alt="error"
           />
         </div>
-      </div>
+        {/* <div className="downloadResumeButton"> */}
+        <div className="intoDesciption">
+          <h1>Hello !</h1>
+          <h1> I am Mrinal Kasyap</h1>
+          <h1> JavaScript Developer</h1>
+          <h1> Developing the change U I need ...</h1>
+        </div>
+
+        {/* </div> */}
+      </section>
       <div className="intoSection">
         <div className="careerbg">
           <h3>
