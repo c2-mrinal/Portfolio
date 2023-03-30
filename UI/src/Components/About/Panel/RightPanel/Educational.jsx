8adb2e5a6174a37bdf970d15cb7b510aac7a7603
c2-mrinal <mrinalspec@gmail.com>
2023-03-31 01:25:24 +0530

    exprianced field added

--- UI/src/Components/About/Panel/RightPanel/Educational.jsx
+++ UI/src/Components/About/Panel/RightPanel/Educational.jsx
@@ -1,80 +1,74 @@
 import React from "react";
-import "./rightPanel.css"
+import "./rightPanel.css";
 
 export default function Educational(props) {
-  const { data } =  props;
-  return (
-    <div>
-      <div className="card">
-        <div className="card-header row">
-        <div className="col-xs-3 col-md-3">
-          <img src={data?.img} height="150" width="150" alt="" />
-    </div>
-          <div className="col-xs-9 col-md-9">
-            {data &&
-              data.info?.map((info) => {
-                return (
-                  <span
-                    className="infoTextBox"
-                    onClick={() => props.showDetails(info[1])}
-                    key={info[1]}
-                  >
-                    <strong>{info[0]} </strong>
-                  </span>
-                );
-              })}
-            {props.showDetail ? (
-              <div className="detailsStyled"> {props.showDetail}</div>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-        <div className="card-body rightPanel">
-          <div>
-            <h5 className="card-title">About</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-          </div>
-          <div>
-            <div>
-              {
-                data?.projects ? 
-                <>
-                <h1> Projects</h1>
-                {
-                  data.projects.map(data=>{
-                    console.log(data);
-                  return <>
-                  <h2><strong>{data.name}</strong></h2>
-                  <div>
-                    <h3> Descriptions</h3>
-                    {data.description}
-                  </div>
-                  <div>
-                    <span><strong>Stacked Used: </strong></span>
-                    <span>{
-                      Object.entries(data.stackUsed).map(val=>{
-                        return <span className="mg-10"><i className={ val[1]}></i> {val[0]}</span>
-                      })
-                      }
-                    </span>
-                  </div>
-                  <br/>
-                  </>
-                    
-                  })
-                }
-                </>
-                : ""
-              }
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const { data } = props;
+	return (
+		<div>
+			<div className="card">
+				<div className="card-header row">
+					<div className="col-xs-3 col-md-3">
+						<img src={data?.img} height="150" width="150" alt="" />
+					</div>
+					<div className="col-xs-9 col-md-9">
+						{data &&
+							data.info?.map((info) => {
+								return (
+									<span className="infoTextBox" onClick={() => props.showDetails(info[1])} key={info[1]}>
+										<strong>{info[0]} </strong>
+									</span>
+								);
+							})}
+						{props.showDetail ? <div className="detailsStyled"> {props.showDetail}</div> : ""}
+					</div>
+				</div>
+				<div className="card-body rightPanel">
+					<div>
+						<h5 className="card-title">About</h5>
+						<p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					</div>
+					<div>
+						<div>
+							{data?.projects ? (
+								<>
+									<h1> Projects</h1>
+									{data.projects.map((data) => {
+										console.log(data);
+										return (
+											<>
+												<h2>
+													<strong>{data.name}</strong>
+												</h2>
+												<div>
+													<h3> Descriptions</h3>
+													{data.description}
+												</div>
+												<div>
+													<span>
+														<strong>Stacked Used: </strong>
+													</span>
+													<span>
+														{Object.entries(data.stackUsed).map((val) => {
+															return (
+																<span className="mg-10">
+																	<i className={val[1]}></i> {val[0]}
+																</span>
+															);
+														})}
+													</span>
+												</div>
+												<br />
+											</>
+										);
+									})}
+								</>
+							) : (
+								""
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- UI/src/Components/About/Panel/RightPanel/Experiance.jsx
+++ UI/src/Components/About/Panel/RightPanel/Experiance.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import "./rightPanel.css";
 
-function Experiance() {
-  return (
-    <div>Experiance</div>
-  )
+function Experiance(props) {
+	const { data } = props;
+	return (
+		<>
+			{data && (
+				<>
+					<div className="experianceTop">
+						<span className="mg-10">
+							<img src={data.img} alt="" height={80} width={80} />
+						</span>
+						<span className="mg-10">
+							<strong> {data.name}</strong>
+						</span>
+						<span className="mg-10">
+							<a href={data.website} target="_blank">
+								<i className="fa-solid fa-arrow-up-right-from-square" title={`${data.name} Website`}></i>
+							</a>
+						</span>{" "}
+						<span className="mg-10">
+							<a href={data.linkedin} target="_blank">
+								<i className="fa-brands fa-linkedin" title={`${data.name} Linkedin`}></i>
+							</a>
+						</span>
+					</div>
+					<div className="row">
+						<div className="col-xs-7 col-md-7">
+							<div>Overview</div>
+							<div>{data.about}</div>
+						</div>
+						<div className="col-xs-5 col-md-5">
+							<div>Role</div>
+							{data.designation && (
+								<div>
+									{data.designation}, {data.department}
+								</div>
+							)}
+							{data.startDate && (
+								<div>
+									{data.startDate} - {data.endDate}
+								</div>
+							)}
+						</div>
+					</div>
+				</>
+			)}
+		</>
+	);
 }
 
-export default Experiance
+export default Experiance;
