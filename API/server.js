67a63eebbf0acf9643cf17b7c79d6ae1ea820653
c2-mrinal <mrinalspec@gmail.com>
2023-08-23 12:21:31 +0530

    heroku deployment

--- API/server.js
+++ API/server.js
@@ -4,12 +4,19 @@ require("dotenv").config();
 
 const app = express();
 const path = require("path");
-const Port = process.env.PORT;
-const publicDirPath = path.join(__dirname, "../src");
+const Port = process.env.PORT || 3333;
+const publicDirPath = path.join(__dirname, "../UI/build");
+console.log({ publicDirPath });
 app.use(express.static(publicDirPath));
 
 app.use(express.json());
 app.use("/api", appRoutes);
+
+app.get("*", (req, res) => {
+	req.sendFile(path.resolve(__dirname, publicDirPath, "index.html"));
+});
+
+// app.use(express.static(""));
 
 app.listen(Port, (error) => {
 	if (error) {
